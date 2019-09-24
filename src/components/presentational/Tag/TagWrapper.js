5abea2d4dda7e3ecc0421fecc863f076eaba9f82
Yacine Chibane-Devereux <y.chibane@me.com>
2019-09-24 19:25:54 +0200

    edited tag style

--- src/components/presentational/Tag/TagWrapper.js
+++ src/components/presentational/Tag/TagWrapper.js
@@ -5,8 +5,11 @@ const TagWrapper = styled.li`
   border: 1px #ececec;
   border-radius: 30px;
   color: #777;
+  display: flex;
   font-size: 11px;
+  justify-content: center;
   margin: 2px 5px;
+  padding: 2px 5px;
 `;
 
 export default TagWrapper;
