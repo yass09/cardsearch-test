3ecc4b06ab72c2c0409b0d37c7d9426395d6d142
Yacine Chibane-Devereux <y.chibane@me.com>
2019-09-24 19:15:38 +0200

    created tag styled component

---
+++ src/components/presentational/Tag/TagWrapper.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const TagWrapper = styled.li`
+  background: #fcfcfc;
+  border: 1px #ececec;
+  border-radius: 30px;
+  color: #777;
+  font-size: 11px;
+  margin: 2px 5px;
+`;
+
+export default TagWrapper;
