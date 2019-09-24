f27604b298d03c1c12f7821ca72fa414505a744f
Yacine Chibane-Devereux <y.chibane@me.com>
2019-09-24 17:29:30 +0200

    set up wrapper for content

---
+++ src/components/presentational/ContentWrapper.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const ContentWrapper = styled.main`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: auto;
+  margin: 10px 30px;
+  width: 100%;
+`;
+
+export default ContentWrapper;
