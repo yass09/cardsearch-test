de65fc8df550a62eef30390d8526b8ba570cf7c9
Yacine Chibane-Devereux <y.chibane@me.com>
2019-09-24 17:31:31 +0200

    added card wrapper styled component, need dimensions

---
+++ src/components/presentational/CardWrapper.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const CardWrapper = styled.li`
+  box-shadow: 0 10px 20px 0 rgba(0, 0, 0, 0.05);
+  border: 1px #f5f5f5;
+  margin: 8px;
+  padding: 10px;
+  list-style: none;
+`;
+
+export default CardWrapper;
