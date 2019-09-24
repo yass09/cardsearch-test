701bb917c9290be1aeca213e3277f4f1014b3898
Yacine Chibane-Devereux <y.chibane@me.com>
2019-09-24 17:30:23 +0200

    edited app wrapper after adding content wrapper

--- src/components/presentational/AppWrapper.js
+++ src/components/presentational/AppWrapper.js
@@ -4,8 +4,7 @@ const AppWrapper = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  justify-content: center;
 `;
 
 export default AppWrapper;
