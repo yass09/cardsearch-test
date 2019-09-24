617cd911320f0af5b43f205ba9405dea3867be3d
Yacine Chibane-Devereux <y.chibane@me.com>
2019-09-24 19:32:16 +0200

    added tag list styled component

---
+++ src/components/presentational/TagList/TagListWrapper.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const TagListWrapper = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  padding: 0;
+`;
+
+export default TagListWrapper;
