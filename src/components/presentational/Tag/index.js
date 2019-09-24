e78e7e85c09d47943345fc198e5a2c81401a36d2
Yacine Chibane-Devereux <y.chibane@me.com>
2019-09-24 19:50:06 +0200

    added proptype to tag component

--- src/components/presentational/Tag/index.js
+++ src/components/presentational/Tag/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import TagWrapper from './TagWrapper';
 
 const Tag = ({ tag }) => {
   return <TagWrapper tag={tag}>{tag}</TagWrapper>;
+};
+
+Tag.propTypes = {
+  tag: PropTypes.string.isRequired,
 };
 
 export default Tag;
