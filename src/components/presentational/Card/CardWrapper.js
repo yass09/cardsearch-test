c24e4848da6b813462350bf1b9814a643fa1ba43
Yacine Chibane-Devereux <y.chibane@me.com>
2019-09-25 17:47:26 +0200

    further adjustments to card style

--- src/components/presentational/Card/CardWrapper.js
+++ src/components/presentational/Card/CardWrapper.js
@@ -1,15 +1,21 @@
 import styled from 'styled-components';
 
 const CardWrapper = styled.li`
+  align-items: center;
   box-shadow: 0 10px 20px 0 rgba(0, 0, 0, 0.05);
   border: 1px #f5f5f5;
+  display: flex;
+  flex-basis: 22%;
+  flex-direction: column;
+  height: 90px;
+  justify-content: space-between;
+  list-style: none;
   margin: 8px;
   padding: 10px;
-  list-style: none;
-  flex-basis: 22%;
   max-width: 350px;
   @media screen and (max-width: 768px) {
     flex-basis: 44%;
+    height: 70px;
   }
   @media screen and (max-width: 480px) {
     flex-basis: 90%;
