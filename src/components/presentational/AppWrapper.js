b1bf613569531e3229789b3836ed043c03cb94c5
Yacine Chibane-Devereux <y.chibane@me.com>
2019-09-25 19:24:11 +0200

    fixed display issue on mobile and tablet

--- src/components/presentational/AppWrapper.js
+++ src/components/presentational/AppWrapper.js
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 
 const AppWrapper = styled.div`
-  width: 100%;
-  height: 100vh;
+  align-items: center;
   display: flex;
   justify-content: center;
-  align-items: center;
+  height: 100vh;
+  width: 100%;
+  @media screen and (max-width: 1024px) {
+    height: auto;
+    min-height: -webkit-fill-available;
+  }
 `;
 
 export default AppWrapper;
